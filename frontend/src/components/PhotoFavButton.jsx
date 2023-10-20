5fe70b04e279112e3e14c39f9af20415a1b93808
AnaBykova <anabykovaca@gmail.com>
2023-10-19 20:29:46 -0600

    added useReducer

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,10 +1,10 @@
 import React, { useCallback, useState, useEffect } from 'react';
-
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton(props) {
 
+  // State to track whether the photo is liked
   const [isLiked, setIsLiked] = useState(false);
   const updateFavorites = props.updateFavorites;
   const photoID = props.photoID;
@@ -13,11 +13,13 @@ function PhotoFavButton(props) {
   const handleLike = useCallback(() => {
     console.log(props);
     console.log(`Before toggle: isLiked=${isLiked}`);
+    // setIsLiked((prevIsLiked) => !prevIsLiked);
     setIsLiked(!isLiked);
-    console.log(`After toggle: isLiked=${!isLiked}`);
+    // setIsLiked(props.toggleFavourite);
     updateFavorites(props.photo);
   });
-
+  
+  // Use useEffect to log the updated value of isLiked
   useEffect(() => {
     console.log({ isLiked });
   }, [isLiked]);
@@ -25,6 +27,7 @@ function PhotoFavButton(props) {
   return (
     <div className="photo-list__fav-icon" onClick={handleLike} >
       <div className="photo-list__fav-icon-svg">
+        {/* Use the FavIcon component with onClick handler */}
         <FavIcon displayAlert={false} selected={isLiked} />
       </div>
     </div>
